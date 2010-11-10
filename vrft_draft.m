99adb1365fcb3696ec6eb8f1ed9f6f0f148d0f5c
Neuhaus <tassianors@gmail.com>
2010-11-09 22:45:19 -0200

    made modifications sugested by Bazanella - we are almost there. =)

--- vrft_draft.m
+++ vrft_draft.m
@@ -4,29 +4,28 @@
 % tassianors@gmail.com
 %==========================================================================
 clear all; close all;
-syms a b
 
 % Time sample [s]
 Ts=10e-3;
 % Final time [s]
 Tf=6;
 
+a=2;b=0.85;c=0.6;
 % Plant's transfer function - unknown in a real word
-Gc=zpk([],[-1],1);
-%Gc_wanted=zpk([],[-10],1)
-Gc_wanted=tf([1 10],[1 1 11]);
-Gd=c2d(Gc, Ts, 'tustin');
-% M is the desired transfer function
-M=c2d(Gc_wanted, Ts, 'tustin')
-
+G=zpk([],[b],a, Ts)
+% M is the desired transfer function in Closed Loop
+M=zpk([],[c],1-c, Ts)
 % Time vector
 t=[0:Ts:Tf];
 
+beta=[ tf([1],[1 -1], Ts); tf([1 0],[1 -1], Ts)]
 % input signal - Random
-u=rand(size(t,2),1, 'single');
-%[Y, T]=step(Gd);
+u1=rand(size(t,2),1);
+m=mean(u1);
+s=std(u1);
+u=(u1-m)/s;
 % response of unknown plant to u input
-Y=lsim(Gd, u, t);
+Y=lsim(G, u, t);
 
 % L filter
 L=M*(1-M);
@@ -36,31 +35,30 @@ yl=lsim(L, Y, t);
 ul=lsim(L, u, t);
 %plot(T,ul,'-g',T,yl,'-r');figure;
 % rl_t = (1/M) *yl
-rl=lsim(1/M,yl, t);
-
+%rl=lsim(M,yl, t);
+rl=filter([1 -c], [1-c], yl);
 % entrada do controlador
 el=rl-yl;
 % saida do contolador
 yl;
-
+plot(t, el);figure;
 % modelo do controlador
-teta=[a; b; 1];
+teta=[a; b];
 % e entrada u saida do controlador
 %phy=[e(t); -e(t-1); u(t-1)]
 N=size(t,2);
 n=size(teta, 1);
 phy=zeros(N, n);
-
-for t=2:N
-    phy(t, 1)=el(t);
-    phy(t, 2)=-el(t-1);
-    phy(t, 3)=yl(t-1);
-end
+phy=lsim(beta, el, t);
+% for t=2:N
+%     phy(t, 1)=el(t);
+%     phy(t, 2)=-el(t-1);
+%     phy(t, 3)=yl(t-1);
+% end
 
 % make sure, rank(phy) = n :)
-teta_r=inv(phy'*phy)*phy'*yl
-kc=teta_r(2);
-Ti=(kc*Ts/teta_r(1))/(1-kc/teta_r(1));
-z = TF('z',Ts);
-Cr=kc*(1+Ts/(Ti*(1-z^(-1))))
+teta_r=inv(phy'*phy)*phy'*ul
 
+C=teta_r'*beta;
+
+T=C*G/(1+C*G)
